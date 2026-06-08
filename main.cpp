33125f7313088cd199258da361e78f98043ab650
staring-hamster <sebastian.ca.esp@gmail.com>
2026-06-07 22:10:57 -0400

    Added Vector2D remaining operators

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
-#define width 200
-#define height 80
+#define WIDTH 200
+#define HEIGHT 80
+
+#define MAX_X WIDTH - 1
+#define MAX_Y HEIGHT - 1
 
 class Vector2D{
 	public:
@@ -12,29 +15,33 @@ class Vector2D{
 		this->x = x;
 		this->y = y; 	
 	}
-
+	Vector2D operator+(Vector2D &add){
+		return Vector2D(this->x + add.x, this->y + add.y);
+	}
 	Vector2D operator-(Vector2D &sub){
 		return Vector2D(this->x - sub.x, this->y - sub.y);
 	}
-
-	Vector2D operator/(float &divisor){
+	Vector2D operator/(double &divisor){
 		return Vector2D(this->x / divisor, this->y / divisor);
+	}
+	Vector2D operator*(double &multiplier){
+		return Vector2D(this->x * multiplier, this->y * multiplier);
 	}
 };
 
-bool matrix[height][width];
+bool matrix[HEIGHT][WIDTH];
 
 void initMatrix(){
-	for (int f = 0; f < height; f++){ // Filas
-		for (int c = 0; c < width; c++){ // Dentro de la fila
+	for (int f = 0; f < HEIGHT; f++){ // Filas
+		for (int c = 0; c < WIDTH; c++){ // Dentro de la fila
 			matrix[f][c] = false;
 		}
 	}
 }
 
 void printMatrix(){
-	for (int f = 0; f < height; f++){ // Eje Y
-		for (int c = 0; c < width; c++){ // Eje X
+	for (int f = 0; f < HEIGHT; f++){ // Eje Y
+		for (int c = 0; c < WIDTH; c++){ // Eje X
 			if (matrix[f][c]){
 				std::cout << "▄ ";
 			}
@@ -47,15 +54,16 @@ void printMatrix(){
 }
 
 void setBit(int x, int y, bool value){
-	matrix[y][x] = value;
+	if ((x >= 0 and x <= MAX_X) and (y >= 0 and y <= MAX_Y)){
+		matrix[y][x] = value;	
+	}
 }
 
-void drawLine(Vector2D start, Vector2D end){
-	Vector2D originVector = end - start;
+void drawLine(Vector2D startPoint, Vector2D endPoint){
+	Vector2D originVector = endPoint - startPoint;
 	for (double i = 0.01; i < 1.0; i+= 0.01){
-		double x = (originVector.x * i) + start.x;
-		double y = (originVector.y * i) + start.y;
-		setBit((int)x, (int)y, true);
+		Vector2D scaledPoint = (originVector * i) + startPoint;
+		setBit((int)scaledPoint.x, (int)scaledPoint.y, true);
 	}
 }
 
